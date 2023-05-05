cd3e9e7febee67fc61ca6a558b63cdea9bdd07ec
Tiago Azevedo <56261720+tiagoazeved0@users.noreply.github.com>
2023-05-05 15:25:05 +0200

    First commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
+	
